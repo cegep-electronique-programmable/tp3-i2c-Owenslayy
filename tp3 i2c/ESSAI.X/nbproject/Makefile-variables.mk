65d8a2bc9f54b6b25643d7cd04dd08163c3e4c12
Owenslayy <e2288053@cmaisonneuve.qc.ca>
2025-03-04 00:05:22 -0500

    Add files via upload
    Ce travail ne marche pas pour la partie de la memoire.

---
+++ tp3 i2c/ESSAI.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ESSAI.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ESSAI.X.production.hex
